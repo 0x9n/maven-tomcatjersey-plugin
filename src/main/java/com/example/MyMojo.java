19f5a59339fd424ed78054c350651943da7cc245
kurokawa.satoshi <tie303802@S1404C10033-T1.tisnt.tis.co.jp>
2015-10-07 09:39:27 +0900

    Configure version

--- src/main/java/com/example/MyMojo.java
+++ src/main/java/com/example/MyMojo.java
@@ -5,10 +5,17 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 
+/**
+ * Run "run" goal
+ */
 @Mojo(name = "run")
 class MyMojo extends AbstractMojo {
 
-
+    /**
+     * Execute "run" goal
+     * @throws MojoExecutionException Unexpected Exception
+     * @throws MojoFailureException Expected Exception
+     */
     public void execute() throws MojoExecutionException, MojoFailureException {
         getLog().info("Hello, world from maven plugin!");
     }
